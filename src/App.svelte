19fdca4daf363b5889c4abc94176aff8dc433655
Paul Kleinschmidt <paul.kleinschmidt1@gmail.com>
2021-01-05 17:29:48 -0800

    Layout WIP

--- src/App.svelte
+++ src/App.svelte
@@ -1,30 +1,29 @@
 <script>
-	export let name;
+	import Welcome from './screens/Welcome.svelte'
+	export let state = 'welcome';
+
+	const start = (e) => {
+		console.log(e);
+	};
 </script>
 
 <main>
-	<h1>Hello {name}!</h1>
-	<p>Visit the <a href="https://svelte.dev/tutorial">Svelte tutorial</a> to learn how to build Svelte apps.</p>
+	{#if state === 'welcome'}
+		<Welcome on:select={start}/>
+	{:else if state === 'playing'}
+		<p>Playing screen goes here</p>
+	{/if}
 </main>
 
 <style>
 	main {
 		text-align: center;
 		padding: 1em;
-		max-width: 240px;
+		max-width: 800px;
 		margin: 0 auto;
-	}
-
-	h1 {
-		color: #ff3e00;
-		text-transform: uppercase;
-		font-size: 4em;
-		font-weight: 100;
-	}
-
-	@media (min-width: 640px) {
-		main {
-			max-width: none;
-		}
+		height: 100%;
+		display: flex;
+		flex-direction: column;
+		justify-content: center;
 	}
 </style>
